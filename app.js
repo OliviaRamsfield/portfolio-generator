46ea7ca37b6296ad36b53ed1eba0a780854e6fa3
Olivia <ramsfol@gmail.com>
2022-07-17 18:04:19 -0500

    getting ready for next module

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-//same as .forEach()
-const printProfileDataArr = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+//MODULE 9.1 CODE EXAMPLE
+// //same as .forEach()
+// const printProfileDataArr = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log("=============");
+//     console.log("=============");
 
-// .forEach() is equal to a for loop to iterate through an array but ONLY works for arrays
-profileDataArr.forEach(profileItem => console.log(profileItem));
+// // .forEach() is equal to a for loop to iterate through an array but ONLY works for arrays
+// profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
+// };
 
-printProfileDataArr(profileDataArgs);
+// printProfileDataArr(profileDataArgs);
