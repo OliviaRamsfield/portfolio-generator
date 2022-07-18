eaaab0ca6f103392fa7736d1fde41b6d531c9dc8
Olivia <ramsfol@gmail.com>
2022-07-17 19:29:59 -0500

    generating HTML page successful

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
 //MODULE 9.1 CODE EXAMPLE
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 // //same as .forEach()
 // const printProfileDataArr = profileDataArr => {
 //     for (let i = 0; i < profileDataArr.length; i++) {
@@ -16,3 +15,21 @@ console.log(profileDataArgs);
 // };
 
 // printProfileDataArr(profileDataArgs);
+
+// ACTUAL CODE FOR PROJECT
+// activate file system module in node.js
+const fs = require("fs");
+
+//connect to export page-template code
+const generatePage = require("./src/page-template");
+
+const profileDataArgs = process.argv.slice(2);
+
+const [names, github] = profileDataArgs;
+
+//generating new file called index.html with the variables names and github and catching error name/message or success
+fs.writeFile("index.html", generatePage(names, github), err => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
