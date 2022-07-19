8dec9fbc684e0bcf36a86898e397b0189723e7ec
Olivia <ramsfol@gmail.com>
2022-07-18 19:24:57 -0500

    added questions for user

--- app.js
+++ app.js
@@ -18,15 +18,74 @@
 
 // ACTUAL CODE FOR PROJECT
 const inquirer = require("inquirer");
-inquirer
-    .prompt([
+const promptUser = () => {
+return inquirer.prompt([
         {
             type: "input",
             name: "name",
             message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your Github username:"
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
         }
     ])
-    .then(answers => console.log(answers));
+}
+
+const promptProject = () => {
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project. (Required)"
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all that apply)",
+            choices: ["Javascript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)"
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            deafult: false
+        }
+    ]);
+};
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
+
 // activate file system module in node.js
 // const fs = require("fs");
 // //connect to export page-template code
